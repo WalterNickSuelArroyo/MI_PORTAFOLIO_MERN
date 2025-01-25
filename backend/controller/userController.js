e245febc9f28622f86e39a216b9bfa72f9f816f1
WalterNickSuelArroyo <nicodehero21@outlook.com>
2025-01-24 19:40:49 -0500

    functions getUser, updateProfile, updatePassword, getUserPortfolio, forgotPassword, resetPassword

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,6 +3,8 @@ import ErrorHandler from '../middlewares/error.js';
 import { User } from '../models/userSchema.js';
 import { v2 as cloudinary } from 'cloudinary';
 import { generateToken } from '../utils/jwtToken.js';
+import { sendEmail } from '../utils/sendEmail.js';
+import crypto from 'crypto';
 
 export const register = catchAsyncErrors(async (req, res, next) => {
     if (!req.files || Object.keys(req.files).length === 0) {
@@ -98,4 +100,171 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
         success: true,
         message: "Logged out",
     });
+});
+
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
+export const updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        fullName: req.body.fullName,
+        email: req.body.email,
+        phone: req.body.phone,
+        aboutMe: req.body.aboutMe,
+        portfolioURL: req.body.portfolioURL,
+        githubURL: req.body.githubURL,
+        linkedinURL: req.body.linkedinURL,
+        youtubeURL: req.body.youtubeURL,
+        facebookURL: req.body.facebookURL,
+    }
+
+    if (req.files && req.files.avatar) {
+        const avatar = req.files.avatar;
+        const user = await User.findById(req.user.id);
+        const profileImageId = user.avatar.public_id;
+        await cloudinary.uploader.destroy(profileImageId);
+        const cloudinaryResponse = await cloudinary.uploader.upload(avatar.tempFilePath, { folder: 'AVATARS' });
+        newUserData.avatar = {
+            public_id: cloudinaryResponse.public_id,
+            url: cloudinaryResponse.secure_url,
+        }
+    }
+
+    if (req.files && req.files.resume) {
+        const resume = req.files.resume;
+        const user = await User.findById(req.user.id);
+        const resumeId = user.resume.public_id;
+        await cloudinary.uploader.destroy(resumeId);
+        const cloudinaryResponse = await cloudinary.uploader.upload(resume.tempFilePath, { folder: 'MY_RESUME' });
+        newUserData.resume = {
+            public_id: cloudinaryResponse.public_id,
+            url: cloudinaryResponse.secure_url,
+        }
+    }
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Profile updated successfully",
+        user,
+    });
+});
+
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const { currentPassword, newPassword, confirmNewPassword } = req.body;
+
+    const user = await User.findById(req.user.id).select('+password');
+
+    if (!currentPassword || !newPassword || !confirmNewPassword) {
+        return next(new ErrorHandler('Please enter all fields', 400));
+    }
+
+
+    const isPasswordMatched = await user.comparePassword(currentPassword);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Password is incorrect', 400));
+    }
+
+    if (newPassword !== confirmNewPassword) {
+        return next(new ErrorHandler('Password does not match', 400));
+    }
+
+    user.password = newPassword;
+
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        message: 'Password updated successfully',
+    });
+});
+
+export const getUserForPortfolio = catchAsyncErrors(async (req, res, next) => {
+    const id = "67933d3642081838487f87a5";
+    const user = await User.findById(id);
+
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
+// FORGOT PASSWORD
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler('User not found with this email', 404));
+    }
+
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    const resetPasswordUrl = `${process.env.DASHBOARD_URL}/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is as follow:\n\n${resetPasswordUrl}\n\nIf you have not requested this email, then ignore it.`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Portfolio Password Recovery',
+            message,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`,
+        });
+    }
+    catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
+// RESET PASSWORD
+
+export const resetPassword = catchAsyncErrors(async (req, res, next) => {
+    const { token } = req.params;
+    const resetPasswordToken = crypto.createHash('sha256').update(token).digest('hex');
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() },
+    });
+
+    if (!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400));
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400));
+    }
+
+    user.password = await req.body.password;
+
+    user.resetPasswordToken = undefined;
+
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    generateToken(user, "Reset Password Successfully", 200, res);
 });
--- backend/router/userRoutes.js
+++ backend/router/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, register } from '../controller/userController.js';
+import { forgotPassword, getUser, getUserForPortfolio, login, logout, register, resetPassword, updatePassword, updateProfile } from '../controller/userController.js';
 import { isAuthenticated } from '../middlewares/auth.js';
 
 const router = express.Router();
@@ -7,6 +7,12 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
+router.get("/me", isAuthenticated, getUser);
+router.put("/update/me", isAuthenticated, updateProfile);
+router.put("/update/password", isAuthenticated, updatePassword);
+router.get("/me/portfolio", getUserForPortfolio);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
 
 
 export default router;
